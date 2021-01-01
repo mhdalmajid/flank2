93769fd7f0f163c8513c4b0f84507b52dbcde9b6
Mohammed Almajid <hamodey85@gmail.com>
2021-01-01 19:44:43 +1100

    added animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,60 @@
-/* eslint-disable no-alert */
-/* eslint-disable no-shadow */
-/* eslint-disable no-use-before-define */
-/* eslint-disable no-undef */
-/* eslint-disable func-names */
-// let googleUser = {}
-// let startApp = function () {
-//   gapi.load('auth2', function () {
-//     // Retrieve the singleton for the GoogleAuth library and set up the client.
-//     auth2 = gapi.auth2.init({
-//       client_id: 'YOUR_CLIENT_ID.apps.googleusercontent.com',
-//       cookiepolicy: 'single_host_origin',
-//       // Request scopes in addition to 'profile' and 'email'
-//       // scope: 'additional_scope'
-//     })
-//     attachSignin(document.getElementById('customBtn'))
-//   })
-// }
+const flankText = document.querySelector('.header_flank-small')
+const flankTextBig = document.querySelector('.about_flank-small')
+const headerSection = document.querySelector('.header')
+const aboutSection = document.querySelector('.about')
+const presentation = document.querySelector('.presentation')
+const contact = document.querySelector('.contact')
+const pTagsInpresentation = document.querySelectorAll('.presentation_container > p')
+const pTagsInpresentationLength = pTagsInpresentation.length
+const sections = [presentation, contact]
+let PHeight = screen.height
+const TotalLayers = sections.length
 
-// function attachSignin(element) {
-//   console.log(element.id)
-//   auth2.attachClickHandler(
-//     element,
-//     {},
-//     function (googleUser) {
-//       document.getElementById(
-//         'name'
-//       ).innerText = `Signed in: ${googleUser.getBasicProfile().getName()}`
-//     },
-//     function (error) {
-//       alert(JSON.stringify(error, undefined, 2))
-//     }
-//   )
-// }
+// setting fixed position for sections
+// when the top of section reaches (top 0)
+let whichLayer = 0
+function setFixedPosition(e) {
+  let scrollPosition = window.pageYOffset
 
-// startApp()
+  if (whichLayer >= TotalLayers) return
+  if (scrollPosition >= PHeight) {
+    PHeight *= 2
+    sections[whichLayer].style.position = 'sticky'
+    sections[whichLayer].style.top = 0
+    whichLayer++
+  }
+}
+
+document.addEventListener('scroll', setFixedPosition)
+
+// show .about section when click and hold flank text (mousedown,touchstart)
+function showPage(e) {
+  headerSection.classList.add('hidden')
+  flankTextBig.classList.add('about_flank-big-to-small-animation')
+}
+
+// hide .about section when releasing flank text  (touchend,mouseup)
+function up(e) {
+  headerSection.classList.remove('hidden')
+  flankTextBig.classList.remove('about_flank-big-to-small-animation')
+}
+flankText.addEventListener('mousedown', showPage)
+flankText.addEventListener('touchstart', showPage)
+
+flankText.addEventListener('touchend', up)
+flankText.addEventListener('mouseup', up)
+let count = 0
+function showPTagOnclick(e) {
+  count += 1
+  if (count >= pTagsInpresentationLength) return
+  if (count === pTagsInpresentationLength - 1) document.querySelector('.presentation_arrow').classList.remove('hidden')
+  if (count === 1) {
+    let firstPImages = pTagsInpresentation[count - 1].querySelectorAll('img')
+    firstPImages[0].classList.add('hidden')
+    firstPImages[1].classList.add('hidden')
+  }
+  pTagsInpresentation[count].classList.remove('hidden')
+  pTagsInpresentation[count - 1].querySelector('img').classList.add('hidden')
+}
+
+presentation.addEventListener('click', showPTagOnclick)
